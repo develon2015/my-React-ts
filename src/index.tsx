b708fd1d1462846a3a67ab41684c7131dd58dabc
develon_cyg <gitee@foxmail.com>
2020-12-02 00:27:16 +0800

    useRef

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import { StrictMode, } from 'react';
 import App from './pages/App';
 
-ReactDOM.render(<StrictMode>{ App }</StrictMode>, /* container */ document.querySelector('#app')); // 严格模式
-// ReactDOM.render(App, /* container */ document.querySelector('#app'));
+// ReactDOM.render(<StrictMode>{ App }</StrictMode>, /* container */ document.querySelector('#app')); // 严格模式
+ReactDOM.render(App, /* container */ document.querySelector('#app'));
